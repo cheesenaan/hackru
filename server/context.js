64bc43fe80bcc396ff60b9854c7afdff2ecee591
Ljf227 <ljfrotton@gmail.com>
2023-10-08 02:11:17 -0400

    added a function for timeline alteration

--- server/context.js
+++ server/context.js
@@ -1,5 +1,5 @@
 const context1 = " input : {{'basic information' : 'My name is john and I am 22 years old'}, {'education' : 'I am currently completing my masters degree in  data science'}, {'how do you spend your time' : 'I enjoy playin frisbee golf'}} output: {{'educate' : 'you complete your masters degree'}, {'before you get you first job you break you leg playing frisbee golf'}, {'recover' : 'you recover hastly from you injury'}, {'golf' : 'you stop playin frisbee golf and focus on your career'}, {'return' : 'you return to school and get your doctorate'}}"; 
-const context2 = "{}"; 
+const context2 = "{input : {{'basic information' : 'My name is jose and I am a 45 year old carpenter'}, {'education' : 'I completed high school, but I have a passion for reading'}, {'how is your time spent' : ''}, {}}"; 
 const context3 = "{}";  
 
 module.exports = context1, context2, context3;  
