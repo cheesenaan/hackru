b9deabc1023dced9037c4491ae36a420eee8c7b2
ad-tra <aew31gcmuy@gmail.com>
2023-10-08 07:29:32 -0400

    first commit, it's chunky

---
+++ client/src/app/form/page.js
@@ -0,0 +1,64 @@
+"use client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import TextareaAutosize from "react-textarea-autosize";
+
+export default function Form() {
+	const questions = [
+		"Hello friend, Tell me about your education, the schools you went to, and the important/notable events that have occurred there.",
+		"Question 2 prompt",
+		"Question 3 prompt",
+	];
+
+	const [answers, setAnswers] = useState(questions.map((q) => ""));
+	const [questionCursor, setQuestionCursor] = useState(0);
+	const router = useRouter();
+
+	const decreaseCursor = () => {
+		setQuestionCursor((old) => Math.max(0, --old));
+	};
+	const increaseCursor = () => {
+		if (questionCursor === questions.length - 1) {
+			router.push("report");
+		}
+		setQuestionCursor((old) => Math.min(questions.length - 1, ++old));
+	};
+	const handleTextAreaChange = (e) => {
+		setAnswers((old) => {
+			let newAnswers = [...old];
+			newAnswers[questionCursor] = e.target.value;
+			return newAnswers;
+		});
+	};
+	return (
+		<div className="flex items-center flex-col">
+			<dir className="flex flex-col items-start p-24 justify-between w-1/2">
+				<p className="font-light text-xl">
+					{questions[questionCursor]}
+				</p>
+				<TextareaAutosize
+					className="mt-5 outline-none w-full  border-b-2 border-fuchsia-700 py-2"
+					placeholder="Share your experiences"
+					value={answers[questionCursor]}
+					onChange={handleTextAreaChange}
+				/>
+				<div className="flex flex-row justify-between w-full items-baseline mt-10">
+					<button
+						className="bg-fuchsia-700 text-white p-4 py-1 "
+						onClick={increaseCursor}
+					>
+						{questionCursor === questions.length - 1
+							? "Submit"
+							: "Next ->"}
+					</button>
+					<button
+						className="text-fuchsia-700"
+						onClick={decreaseCursor}
+					>
+						&lt;----
+					</button>
+				</div>
+			</dir>
+		</div>
+	);
+}
---
+++ client/src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Roboto_Mono } from 'next/font/google'
+
+const roboto = Roboto_Mono({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Diversions',
+  description: 'explore your alternative selfs',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={roboto.className + " bg-white"}>{children}</body>
+    </html>
+  )
+}
