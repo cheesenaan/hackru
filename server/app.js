67bbaac39e22d0d59a1743ddae0178dc9e65ec9e
ad-tra <aew31gcmuy@gmail.com>
2023-10-08 11:42:17 -0400

    add cors support on server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 var app = module.exports = express();
+var cors = require('cors');
 const port = 3000
 
 app.use(express.json())
+app.use(cors())
 
 const auth = require('./auth');
 const timeline = require('./timeline');
