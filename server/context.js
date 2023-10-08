349ce57e0383037cc4823123807a0c07735fd659
Ljf227 <ljfrotton@gmail.com>
2023-10-08 11:17:04 -0400

    improve context

--- server/context.js
+++ server/context.js
@@ -1,9 +1,9 @@
 const context1 = `input: 
 {
     {
-        {"title" : "winning karater tornament", "description" : "I went to ABC elementary school and won the karate tornament", "alternateChoices" : "["I assaulted my teacher getting kicked out of school", "I did not attend the karater tornument"]"}, 
-        {"title" : "punched bob in the face", "description" : "I attended ZYZ high school, but I punched bob in the face. He was severly injured leading to jail time", "alternateChoice" : "["I did not punch bob in the face", "I punched joe in the face"]"}, 
-        {"title" : "in jail", "description" : "In jail I was extremly well behaved. I stayed out of a fight", }
+        {"title" : "winning karater tornament", "description" : "I went to ABC elementary school and won the karate tornament", "alternateChoices" : ["I assaulted my teacher getting kicked out of school", "I did not attend the karater tornument"]}, 
+        {"title" : "punched bob in the face", "description" : "I attended ZYZ high school, but I punched bob in the face. He was severly injured leading to jail time", "alternateChoice" : ["I did not punch bob in the face", "I punched joe in the face"]}, 
+        {"title" : "in jail", "description" : "In jail I was extremly well behaved. I stayed out of a fight", "alternateChoices" : ["I was not well behaved and got time added", "I did not stay out of the fight and got arm broken"]},
     },
 
     {
@@ -13,7 +13,12 @@ const context1 = `input:
 
 output : 
 
-{}
+{
+    {"title" : "winning karater tornament", "description" : "I went to ABC elementary school and won the karate tornament", "alternateChoices" : ["I assaulted my teacher getting kicked out of school", "I did not attend the karater tornument"]},
+    {"title" : "I held back from hurting bob", "description": "I went to ZYZ high school and did not punch bob. I was a high achiever", "alternateChoices" : ["I punched joe in the face", "I punched bob in the face]},
+    {"title" : "I am an overachevier", "description" : "I attended harvard university. I also stayed away for Jane", "alternateChoices" : ["I did not stay away from Jane", "I dropped out of harvard"]},
+
+}
 
 `; 
 
@@ -25,21 +30,18 @@ const context6 = `input :
     "employment": "I am currently working as a software developer at XYZ Company. I enjoy my job, but I wish I had more opportunities for professional growth. In the past, I worked at DEF Corporation as a junior developer, where I gained valuable experience.",
     "socialLife": "My social life is quite active. I have a close-knit group of friends, and we often organize get-togethers and outings. One notable event was a recent trip to the mountains with my friends.",
     "loveLife": "I'm currently in a happy relationship with my partner. In the past, I've had a few relationships that didn't work out due to differences in goals and values. I wish some of those relationships had lasted longer.",
-    "freeLife": "In my free time, I enjoy reading, hiking, and playing the guitar. These activities help me relax and unwind after a busy workweek.",
-    "religionAndBeliefs": "I consider myself agnostic and open to various spiritual beliefs. I believe in treating others with kindness and respect. This belief has guided me in making ethical decisions in my personal and professional life.",
     "wishYouNeverDid": "One thing I wish I never did was procrastinate on an important project, which resulted in a lot of stress and missed deadlines.",
-    "wishYouDid": "I wish I had taken a gap year to travel and explore different cultures before starting my career. It's something I've always wanted to do."
   }
 
 
   output : 
   
   {
-    {"title" : "Won science fair", "description": "I began with eduaction at XYZ school where I wohn the science fair. I was also raised agnostic"},
-    {"title : "Teenager", "description": "I was quite social but started to develop a closley knit friend group. I dated a bit and went on a trip with my friends. I regret missing an imporant project"},
-    {"title" : "Never went on a trip", "description": "I started to enjoy reading, hiking and guitar playing. I started college, but regret not taking a gap year."},
-    {"title" : "Young adult", "description": "I began a job at DEF corp as a juinor dev after majoring in computer science at ABC university."},
-    {"title" : "Adulthood", "description": "I progressed to a job at XYZ company as a software developer. I am in a happy relaionship but wish more oppurtunites for growth"},
+    {"title" : "Won science fair", "description": "I began with eduaction at XYZ school where I won the science fair.", "alternateChoices" : ["I slept in before the science fair", "I studied harder"]},
+    {"title : "Teenager", "description": "I was quite social but started to develop a closley knit friend group. I dated a bit and went on a trip with my friends. I regret missing an imporant project", "alternateChoices" : ["I did not date", "I did not go on the friend trip"]},
+    {"title" : "Never went on a trip", "description": "I started college, but regret not taking a gap year.", "alternateChoices" : ["I took a gap year", "I joined a fraternity"]},
+    {"title" : "Young adult", "description": "I began a job at DEF corp as a juinor dev after majoring in computer science at ABC university.", "alternateChoices" : ["I majored in business", "I dropped out of college"]},
+    {"title" : "Adulthood", "description": "I progressed to a job at XYZ company as a software developer. I am in a happy relaionship but wish more oppurtunites for growth", "alternateChoices" : ["I broke up with my girlfriend", "I seized the oppurtunites in the office"]},
   }
 
   
