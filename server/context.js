70aa120e80d5975afc45e2560fbb7ac53725bd95
Ljf227 <ljfrotton@gmail.com>
2023-10-08 01:48:57 -0400

    the ai is coming together

---
+++ server/context.js
@@ -0,0 +1,5 @@
+const context1 = " input : {{'basic information' : 'My name is john and I am 22 years old'}, {'education' : 'I am currently completing my masters degree in  data science'}, {'how do you spend your time' : 'I enjoy playin frisbee golf'}} output: {{'educate' : 'you complete your masters degree'}, {'before you get you first job you break you leg playing frisbee golf'}, {'recover' : 'you recover hastly from you injury'}, {'golf' : 'you stop playin frisbee golf and focus on your career'}, {'return' : 'you return to school and get your doctorate'}}"; 
+const context2 = "{}"; 
+const context3 = "{}";  
+
+module.exports = context1, context2, context3;  
--- server/openai.js
+++ server/openai.js
@@ -1,5 +1,5 @@
-const openai = require('./config/openaiConfig')
-
+const openai = require('./config/openaiConfig'); 
+const { context1, context2, context3 } = require('./context'); 
 
 function isJSONString(str) {
     try {
@@ -11,41 +11,54 @@ function isJSONString(str) {
 }
 
 
-
-export default function open(data){
-
+async function open(data){
 
     const recieveprompt = "below you will be reciving json data that contains information about a persons life. do what you must"; 
+    const cont = "here are some examples for you"; 
+    const context = `${context1}`; 
 
-    const upperprompt = "Please output in json format like this {'title': 'description', 'title': 'description'} the description must be less than or equal to 20 tokens and the title must be less than or equal to 3 tokens"; 
-    const midprompt = "Based on the follow data on want you to predict the future of this persons life use should predict in steps. Remeber the formatting"
+    const upperprompt = "Please output in json format like this {'title': 'description', 'title': 'description'} the description must be less than or equal to 44 tokens and the title must be less than or equal to 8 tokens you can add as many steps as you want. minium of 245 total tokens"; 
+    const midprompt = "Based on the follow data on want you to predict the future of this persons life use should predict in steps. Remeber the formatting. also age does not indicate the length of your output. Also be definite in your responses"
 
     const superprompt = "here is the data"
 
     async function getResponse(){
         
-        let response; 
         let isValid = false; 
 
         while(!isValid){
             const completion = await openai.chat.completions.create({
-                messages: [{ role: "system", content: `${upperprompt} \n ${midprompt} \n ${recieveprompt} \n ${superprompt} \n ${data}` }],
+                messages: [{ role: "system", content: `${upperprompt} \n ${midprompt} \n ${recieveprompt} \n ${superprompt} \n ${cont} \n ${context} \n ${data}` }],
                 model: "gpt-3.5-turbo",
-                max_tokens: 255,
+                max_tokens: 600,
 
-                temperature: .2,
+                temperature: .1,
             });
 
-            isValidJSON = isJSONString(response.data.choices[0].text);
+            
 
-            if (!isValidJSON) {
+            isValid = isJSONString(completion.choices[0].message.content);
+            console.log(isValid)
+            //isValid=true
+
+            if (!isValid) {
                 console.log("Response is not valid JSON, retrying...");
             }
+            //console.log(completion.choices[0]);
+            return(completion.choices[0]); 
+            
         }
+
+       
     }
 
-    getResponse(); 
+    return(getResponse()); 
 
 }
+
+(async () => {
+    const result = await open("{'basic informtion': 'my name is john and I am 12'}, {'interests: 'running, swimming and skating'}");
+    console.log(result);
+})();
 
 
